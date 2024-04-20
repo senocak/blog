273243da115808b1f80614ee9e3135e8a292ca0c
anil.senocak <anil.senocak@turkcell.com.tr>
2024-04-20 21:32:29 +0300

    serialization

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "com.github.senocak"
@@ -21,6 +22,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
