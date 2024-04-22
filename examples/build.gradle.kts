bafed7f028885211fcc882c23475a7990ca36f0f
anil.senocak <anil.senocak@turkcell.com.tr>
2024-04-22 16:42:08 +0300

    Tüm response'lara Header ekleme

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
